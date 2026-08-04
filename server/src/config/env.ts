df599824bf50bacb8f528aecbaa30209c9352ff7
Barney (Kyle) <130295277+JamesHickers@users.noreply.github.com>
2026-08-04 05:31:44 +0300

    Create env.ts

---
+++ server/src/config/env.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+
+  PORT: z.coerce
+    .number()
+    .default(3000),
+
+  DATABASE_URL: z.string(),
+
+  JWT_SECRET: z.string()
+    .min(32)
+
+});
+
+export const env =
+  envSchema.parse(process.env);
